092d2096ee62d1bb7a4809c8d41c1531a9273f1c
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-07 23:50:17 -0600

    deploying

--- employeedirectory/src/components/SearchResultContainer.js
+++ employeedirectory/src/components/SearchResultContainer.js
@@ -1,23 +1,36 @@
 import React, { Component } from "react";
 import ResultList from "./ResultList";
 import API from "../utils/API";
+import SearchForm from "./SearchForm";
 
 class SearchResultContainer extends Component {
     state = {
         search: "",
-        results: []
+        results: [],
+        filteredResults: []
     };
 
     componentDidMount() {
         API.getData()
             .then(response => {
-                this.setState({ results: response.data.results, resultsFiltered: response.data.results })
+                this.setState({ results: response.data.results, filteredResults: response.data.results })
             }).catch(err => console.log(err));
     }
+
+    handleInputChange = event => {
+        event.preventDefault();
+        console.log(event);
+        this.setState({ search: event.target.value })
+    };
+
+
 
     render() {
         return (
             <div>
+                <SearchForm
+                    handleInputChange={this.handleInputChange} 
+                    value={this.state.search}/>
                 <ResultList results={this.state.results} />
             </div>
         );
